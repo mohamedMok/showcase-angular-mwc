d52e23e411f9cfd092f82e17facf2468f1ac4a40
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2021-10-12 15:20:27 +0200

    feat(pattern): add showcase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.type = 'selectable'
+    }, 2500);
+  }
   title = 'showcase-angular-mwc';
+  size = 's';
+  // Accordion
+  open = undefined;
+  //Breadcrumb 
+  // breadcrumItems = [{"text":"Level 00","href":"#"},{"text":"Here is the level 01","href":"#"},{"text":"This is another very long level (Level 02) link","href":"#","isActive":true},{"text":"The third level is here","href":"#"},{"text":"This is the last Level link","href":null}];
+  // breItems = undefined;
+  // Tags
+  type='selectable';
+  helloWorld() {
+    alert('Click actif :+1')
+  }
+  handleAccordion() {
+    this.open = this.open === undefined ? true : undefined;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import '@mozaic-ds/mozaic-web-components/public/adeo/bundle.js';
+import '@mozaic-ds/mozaic-web-components/public/icons.js';
 
 import { AppComponent } from './app.component';
 
@@ -7,6 +10,7 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
     BrowserModule
   ],
