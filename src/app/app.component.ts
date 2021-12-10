05d99746b13cc37cf60cbab2ad403aaacc6e6b4f
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2021-12-10 09:49:57 +0100

    add select value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import '@mozaic-ds/mozaic-web-components/public/bundle.js';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,14 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       this.type = 'selectable';
-      console.log(this.opts);
-      console.log(this.opts);
+      this.toggleCheck = false;
+      this.optionSelected = 'option3';
+      console.log(this.optionSelected);
     }, 2500);
+  }
+
+  ngOnChanges(): void {
+    console.log('Somestuff changed');
   }
   title = 'showcase-angular-mwc';
   checkboxValue = true;
@@ -19,23 +25,30 @@ export class AppComponent implements OnInit {
   checkboxtext = 'titit';
   indeterminate = true;
   size = 's';
+  selectOption = 'null';
+  optionSelected = 'option4';
   // Accordion
   open = true;
-  opts = `[
+  optis = [
+  {
+    "text":"Option 1",
+    "value":"option1"
+  },
+  {
+    "text":"Option 2",
+    "value":"option2",
+    "disabled":true
+  },
+  {
+    "text":"Option 3",
+    "value":"option3"
+  },
     {
-       "text":"Option 1",
-       "value":"option1"
-    },
-    {
-       "text":"Option 2",
-       "value":"option2",
-       "disabled":true
-    },
-    {
-       "text":"Option 3",
-       "value":"option3"
-    }
- ]`;
+    "text":"Option 4",
+    "value":"option4"
+  }
+];
+  opts = JSON.stringify(this.optis);
   //Breadcrumb
   // breadcrumItems = [{"text":"Level 00","href":"#"},{"text":"Here is the level 01","href":"#"},{"text":"This is another very long level (Level 02) link","href":"#","isActive":true},{"text":"The third level is here","href":"#"},{"text":"This is the last Level link","href":null}];
   // breItems = undefined;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import '@mozaic-ds/mozaic-web-components/public/bundle.js';
-import Accordion from '@mozaic-ds/mozaic-web-components/public/components/accordion/accordion.js';
-
 import { AppComponent } from './app.component';
 
-if (!customElements.get('m-accordion')) {
-  customElements.define('m-accordion', Accordion);  
-}
-
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-  
-export class AppModule { }
+export class AppModule {}
